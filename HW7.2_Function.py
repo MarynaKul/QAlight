29ac255a2dd63ea7be1f3a3427ffab70bdeb71e0
Maryna <marysjaartemida@gmail.com>
2020-02-01 20:31:34 +0200

    Function 7

--- HW7.2_Function.py
+++ HW7.2_Function.py
@@ -1,9 +1,8 @@
 import random
 def myList(length, maxValue):
-    [random.randint(0, maxValue) for x in range(length)]
+    a = [random.randint(0, maxValue) for x in range(length)]
     for x in range(length):
         if x > 7:
-         print(x)
-        else:
-            print('небольше 7')
+         print(a[x])
+
 
