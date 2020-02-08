cd0d988a66d37054c9d1587f1aa327443f9e6164
Maryna <marysjaartemida@gmail.com>
2020-02-08 18:07:04 +0200

    Function 2 lists

--- HW9.3_Sum of numbers.py
+++ HW9.3_Sum of numbers.py
@@ -1,6 +1,6 @@
-a = input('Your number: ')
-summ = 0
-for i in a:
-     summ = summ + int(i)
-     continue
-print('Sum of Numerals: ', summ)
+def calculate(a):
+      summ = 0
+      for i in str(a):
+           summ = summ + int(i)
+           continue
+      print('Sum of Numerals: ', summ)
