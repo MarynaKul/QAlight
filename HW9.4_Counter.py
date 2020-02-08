3bff9f9d31edcf4e23508573305ccfbb82660fab
Maryna <marysjaartemida@gmail.com>
2020-02-08 19:36:54 +0200

    Counter of symbols

--- HW9.4_Counter.py
+++ HW9.4_Counter.py
@@ -1,6 +1,6 @@
-a = str(input('Введиите текст: '))
-c = str(input('Введите символ: '))
-def b(c):
- global a
- global с
-print('Символ повторяется ', a.count(c), ' раз в строке')
+def counter():
+  a = str(input('Введиите текст: '))
+  c = str(input('Введите символ: '))
+  print('Символ повторяется ', a.count(c), ' раз в строке')
+counter()
+
