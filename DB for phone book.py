fc39e65947a957060aa8c2d0a1f8e729920a5e47
Maryna <marysjaartemida@gmail.com>
2020-03-01 14:17:32 +0200

    DB

--- DB for phone book.py
+++ DB for phone book.py
@@ -2,11 +2,11 @@ import sqlite3
 conn = sqlite3.connect("myDB.db")
 cursor = conn.cursor()
 
-cursor.execute("""Create table PhoneBook
+cursor.execute("""Create table Telephone_Book
                    (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                     first_name TEXT NOT NULL,
                     last_name TEXT,
                     phone TEXT NOT NULL
-                    constraint CK_PhoneBook_phone CHECK (phone LIKE ('[0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]')),
+                    constraint CK_Telephone_Book_phone CHECK (phone LIKE '([0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
                     comment TEXT)""")
 conn.commit()
